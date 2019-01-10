155424a88c752a2809b18520f690d02bce83aa36
Alex Moica <oc3anstudios@gmail.com>
2019-01-10 01:00:11 -0500

    started login page, more stylizing

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from AutoComplete import *
-import os
+import os	
+import urllib.request
 
 dp = os.path.join(os.path.dirname(__file__), '')
 
@@ -8,37 +9,17 @@ dp = os.path.join(os.path.dirname(__file__), '')
 def showFrame(page):
 		page.grid(row=0, column=0, sticky='nsew')
 		page.tkraise()
-
+		
 class HomePage(tk.Frame):
 	def __init__(self, master):
 		
-		logoImage=tk.PhotoImage(file=dp+'LOGO.gif')
+		logoImage=tk.PhotoImage(file=dp+'Target.png')
 		
 		#set up the tk window
 		tk.Frame.__init__(self, master)
 		root.geometry('860x483')
 		root.resizable(width=False, height=False)
 		HomePage.configure(self, bg='white')		
-		
-		for i in range(1, 52): #create spacer label for total columns in the frame
-			tk.Label(self, text=i, bg='black', fg='white', height=1, width=2).grid(row=i, column=0)
-			tk.Label(self, text=i, bg='black', fg='white', height=1, width=2).grid(row=0, column=i)
-			
-		#get all the university names
-		uniFile = open(dp+'universities.txt', 'r')
-		uniList = uniFile.readlines()
-		uniFile.close()
-		
-		#define objects
-		cBtn = tk.Button(self, text="$", fg='green', activeforeground='green', width=10)
-		autoComplete = AutoComplete(uniList, self, width=80)
-		logo = tk.Label(self, image=logoImage, width=500, height=150)
-		logo.image=logoImage #keep reference to image to avoid being cleared by Python's garbage-collector
-		
-		#define placements
-		logo.grid(row=1, column=2, rowspan=8, columnspan=30)
-		autoComplete.grid(row=11, column=1, rowspan=2, columnspan=36)
-		cBtn.grid(row=11, column=20, rowspan=2, columnspan=26)
 		
 		#function to delete placeholder text on field entry
 		def placeholderDelete(event):
@@ -47,11 +28,62 @@ class HomePage(tk.Frame):
 			else:
 				autoComplete.lb_up = False
 			autoComplete.delete(0, 'end')
+		
+		#functiont to check that typed university is in list
+		def uniSelect():
+			if autoComplete.get()=='MCMASTER UNIVERSITY':
+				self.loginWindow() #TODO: Disable button on click, center tk window popup, create 
+			elif autoComplete.get()=='UNIVERSITY OF TORONTO':
+				self.loginWindow()
+			elif autoComplete.get()=='UNIVERSITY OF WATERLOO':
+				self.loginWindow()
+			else:
+				print("Valid university not selected")
+		
+		for i in range(1, 52): #create spacer label for total columns in the frame
+			tk.Label(self, text='', bg='white', fg='white', height=1, width=2).grid(row=i, column=0)
+			tk.Label(self, text='', bg='white', fg='white', height=1, width=2).grid(row=0, column=i)
+		
+		#get all the university names
+		uniFile = open(dp+'universities.txt', 'r')
+		uniList = uniFile.readlines()
+		uniFile.close()
+		
+		#define objects
+		selectBtn = tk.Button(self, text="Select", fg='white', bg='red', bd=0, activeforeground='red', activebackground='lightgrey', width=10, relief='flat', command=uniSelect) #TODO: round corners, font for text, black border, how the button looks like when clicked
+		autoComplete = AutoComplete(uniList, self, width=80)
+		logo = tk.Label(self, image=logoImage, width=165, height=165)
+		logo.image=logoImage #keep reference to image to avoid being cleared by Python's garbage-collector
+		
+		#define placements
+		logo.grid(row=3, column=4, rowspan=8, columnspan=35)
+		autoComplete.grid(row=12, column=1, rowspan=2, columnspan=36)
+		selectBtn.grid(row=12, column=20, rowspan=2, columnspan=26)
 			
 		autoComplete.bind('<Button-1>', placeholderDelete) #clicking inside the entry input will call placeholderDelete and delete the current text
+
+	#function for creating general toplevel login page
+	def loginWindow(self):
+		t = tk.Toplevel(self)
+		t.wm_title("MarkBoard - Login")
+		t.geometry('275x300')
+		t.resizable(width=False, height=False)
+		t.configure(bg='lightgrey')
+		t.focus()
+		
+'''
+class LoginPage(tk.Frame):
+	def __init__(self, master, url):
+		tk.Frame.__init__(self, master)
+		root.wm_title("MarkBoard - Login")
+		root.geometry('275x300')
+		root.resizable(width=False, height=False)
+		root.configure(bg='white')
+		LoginPage.configure(self, bg='white')
+'''
 		
 if __name__ == '__main__': #code only executed to run as a program not when simply imported as a module
 	root = tk.Tk()
-	root.wm_title("whatsmymark?")
+	root.wm_title("MarkBoard - Welcome")
 	showFrame(HomePage(root)) #start with HomePage
 	root.mainloop()
