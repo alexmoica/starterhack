87e13a0f6902bcdb7235c5861209abd246a41077
Ayman Shaikh <aymanshaikh@gmail.com>
2019-01-12 19:31:11 -0500

    added clicking to py plot

---
+++ pyplot_2.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+def make_picker(fig, wedges):
+
+    def onclick(event):
+        wedge = event.artist
+        label = wedge.get_label()
+        print (label)
+
+# Make wedges selectable
+    for wedge in wedges:
+        wedge.set_picker(True)
+
+    fig.canvas.mpl_connect('pick_event', onclick)
+
+# Make an example pie plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+labels = ['1ZA3', '1D03', '1E03', '1ZC3', '1D04', '2CI5']
+weight = []
+
+for i in labels:
+    x = int(i[(len(i)-1):])
+    weight.append(x)
+
+wedges, plt_labels = ax.pie(weight, labels=labels)
+ax.axis('equal')
+
+make_picker(fig, wedges)
+plt.show()
